dd476dd1e5e261dbf3f0084e551333f0f42cb1a6
marcellocurto <marcello@roark.at>
2020-05-12 11:40:52 +0200

    Timestamp

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 const stripe = require('stripe')(process.env.API_KEY);
 const puppeteer = require('puppeteer')
+const dateNow = Date.now()
 
 async function getStripeBalance() {
     await stripe.balance.retrieve((err, balance) => {
         if (err) { return false }
+        balance.date = dateNow
         fs.writeFile('docs/stripe.json', JSON.stringify(balance), 'utf8', (err) => {
             if (err) console.log(err)
         })
         return true
     })
-
 }
 
 getStripeBalance()
@@ -48,18 +49,17 @@ async function getBankBalance() {
     await page.waitForSelector('table')
     const balanceWrapper = await page.$$('[data-label="Kontostand"]')
 
-    balance = balanceWrapper[1]
+    const balance = balanceWrapper[1]
     const balanceValue = await page.evaluate(balance => balance.innerText, balance)
     const balanceClean = balanceValue.split('€')[0].trim()
     const balanceFloat = parseFloat(balanceClean.replace(/\./g, '').replace(/\,/g, '.'))
-
-    let balancesObject = { balances: { account: balanceFloat } };
+    const balancesObject = { balances: { account: balanceFloat, date: dateNow } }
 
     fs.writeFile('docs/bank.json', JSON.stringify(balancesObject), 'utf8', (err) => {
         if (err) console.log(err)
     })
 
-    await page.waitFor(3000)
+    await page.waitFor(2000)
     await page.waitForSelector('#llLink')
     const buttonLogout = await page.$('#llLink')
     await buttonLogout.click()
